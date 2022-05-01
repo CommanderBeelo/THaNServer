f7f945de9a5b61aa5cc01feed4d0742c31f06b4d
CommanderBeelo <xxxbilliamxxx@gmail.com>
2022-05-01 13:53:49 -0400

    Update to current build
    Updated GitHub Code to Current

--- Scripts/config.cpp
+++ Scripts/config.cpp
@@ -15,6 +15,13 @@ class CfgPatches
         requiredVersion = 0.1;
 		requiredAddons[]=
 		{
+			"DZ_Data",
+			"DZ_Characters_Vests",
+			"DZ_Characters_Backpacks",
+			"DZ_Scripts",
+			"DZ_Characters",
+			"JPC_vest",
+			"DZ_Gear_Containers",
 			"DayZExpansion_Scripts"
 		};
 	};
@@ -47,16 +54,17 @@ class CfgPatches
 ///////////////////////////////////////////////
 class CfgMods
 {
-	class DZ_THaNServer_ExpansionLoadingScreen
+	class THaNServer
 	{
 	    dir = "THaNServer"; //! Name of the mod
-		picture = "";
+		picture = "THaNServer/Scripts/bookimage/imagesets/beeloswatermark.edds";
 	    action = "";
-	    hideName = 1;
-	    hidePicture = 1;
+	    hideName = 0;
+	    hidePicture = 0;
 		name = "THaNServer";
 		credits = "CommanderBeelo";
 	    author = "CommanderBeelo";
+		overview="Server mod for The Horror at Namalsk";
 	    authorID = "CommanderBeelo";
 	    version = "1.0";
 	    extra = 0;
@@ -64,7 +72,9 @@ class CfgMods
 
 		dependencies[]= 
 		{
-			"Game"
+			"Game",
+			"Mission",
+			"World"
 		};
 		class defs
 		{
@@ -76,29 +86,7 @@ class CfgMods
 					"THaNServer/Scripts/3_Game"
 					//! Mod name/Scripts/3_Game
 				};
-			};	
-		};
-    };
-	class MainMenuMusicMod
-	{
-		dir="THaNServer";
-		picture="";
-		action="";
-		hideName=1;
-		hidePicture=1;
-		name="THaNServer MainMenuMusicMod";
-		credits="Commander Beelo";
-		author="Commander Beelo";
-		authorID="0";
-		version="1.0";
-		extra=0;
-		type="mod";
-		dependencies[]=
-		{
-			"Mission"
-		};
-		class defs
-		{
+			};
 			class missionScriptModule
 			{
 				value = "";
@@ -108,7 +96,7 @@ class CfgMods
 				};
 			};
 		};
-	};
+    };
 };
 class CfgSoundSets
 {
@@ -124,7 +112,152 @@ class CfgSoundShaders
 {
 	class Main_Music_Menu_SoundShader
 	{
-		samples[] = {{"THaNServer\Scripts\LoadingMusic\MainMenu.ogg",1}};
-		volume = 0.70794576;
+		samples[] = {{"THaNServer\Scripts\LoadingMusic\mainmenu2.ogg",1}};
+		volume = 1.00000000;
+	};
+};
+class cfgVehicles
+{
+	class mk5_vest_base;
+	class MMG_MK_V_Armor_420: mk5_vest_base
+	{
+		scope = 2;
+		displayName = "Dankest MK5 Armor";
+		hiddenSelectionsTextures[] = 
+		{
+			"\THaNServer\scripts\textures\canna2.paa",
+			"\THaNServer\scripts\textures\canna2.paa",
+			"\THaNServer\scripts\textures\canna2.paa"
+		};
+	};
+	
+	class combatshirt_base;
+	class MMG_combatshirt_420: combatshirt_base
+	{
+		scope=2;
+		displayName="Dankest Combat Shirt";
+		hiddenSelectionsTextures[] = 
+		{
+			"\THaNServer\scripts\textures\canna2.paa",
+			"\THaNServer\scripts\textures\canna2.paa",
+			"\THaNServer\scripts\textures\canna2.paa"
+		};
+	};
+	
+	class combatpants_base;
+	class MMG_combatpants_420: combatpants_base
+	{
+		scope=2;
+		displayName="Combat Ready Weed Pants";
+		hiddenSelectionsTextures[] = 
+		{
+			"\THaNServer\scripts\textures\canna2.paa",
+			"\THaNServer\scripts\textures\canna2.paa",
+			"\THaNServer\scripts\textures\canna2.paa"
+		};
+	};
+	
+	class TacticalGloves_ColorBase;
+	class mmg_tactical_gloves_420: TacticalGloves_ColorBase
+	{
+		scope=2;
+		displayName="Dankest Gloves";
+		hiddenSelectionsTextures[] = 
+		{
+			"\THaNServer\scripts\textures\canna2.paa",
+			"\THaNServer\scripts\textures\canna2.paa",
+			"\THaNServer\scripts\textures\canna2.paa"
+		};
+	};
+	
+	class balaclava_base;
+	class MMG_balaclava_420: balaclava_base
+	{
+		scope=2;
+		displayName="Dankest Balaclava";
+		visibilityModifier=0.85000002;
+		hiddenSelectionsTextures[] = 
+		{
+			"\THaNServer\scripts\textures\canna2.paa",
+			"\THaNServer\scripts\textures\canna2.paa",
+			"\THaNServer\scripts\textures\canna2.paa"
+		};
+	};
+	
+	
+	//Arm Patches
+		
+	class mmg_patch_base;
+	class MMG_patch_admin: mmg_patch_base
+	{
+		scope=2;
+		displayName="THaNServer Admin Patch";
+		visibilityModifier=0.85000002;
+		inventorySlot[] = {"mmg_patch", "armband"};
+		hiddenSelectionsTextures[]=
+		{
+			"\THaNServer\scripts\textures\patches\patch_thanadmin.paa",
+			"\THaNServer\scripts\textures\patches\MVS_Armband_co.paa"
+		};
+	};
+	class MMG_patch_deadsun: mmg_patch_base
+	{
+		scope=2;
+		displayName="Deadsun's Patch";
+		visibilityModifier=0.85000002;
+		hiddenSelectionsTextures[]=
+		{
+			"\THaNServer\scripts\textures\patches\patch_deadsun.paa"
+		};
+	};
+	class MMG_patch_infini: mmg_patch_base
+	{
+		scope=2;
+		displayName="Deadsun's Patch";
+		visibilityModifier=0.85000002;
+		hiddenSelectionsTextures[]=
+		{
+			"\THaNServer\scripts\textures\patches\patch_infini.paa"
+		};
+	};
+	class MMG_patch_intgp: mmg_patch_base
+	{
+		scope=2;
+		displayName="Deadsun's Patch";
+		visibilityModifier=0.85000002;
+		hiddenSelectionsTextures[]=
+		{
+			"\THaNServer\scripts\textures\patches\patch_intgp.paa"
+		};
+	};
+	class MMG_patch_intgp2: mmg_patch_base
+	{
+		scope=2;
+		displayName="Deadsun's Patch";
+		visibilityModifier=0.85000002;
+		hiddenSelectionsTextures[]=
+		{
+			"\THaNServer\scripts\textures\patches\patch_intgp2.paa"
+		};
+	};
+	class MMG_patch_potleaf: mmg_patch_base
+	{
+		scope=2;
+		displayName="Deadsun's Patch";
+		visibilityModifier=0.85000002;
+		hiddenSelectionsTextures[]=
+		{
+			"\THaNServer\scripts\textures\patches\patch_potleaf.paa"
+		};
+	};
+	class MMG_patch_psylocke: mmg_patch_base
+	{
+		scope=2;
+		displayName="Deadsun's Patch";
+		visibilityModifier=0.85000002;
+		hiddenSelectionsTextures[]=
+		{
+			"\THaNServer\scripts\textures\patches\patch_psylocke.paa"
+		};
 	};
 };
