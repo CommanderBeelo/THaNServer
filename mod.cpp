ee3496ade424856387123d295346e0dd006ef0b3
CommanderBeelo <xxxbilliamxxx@gmail.com>
2023-06-19 11:49:27 -0400

    Update mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -1,8 +1,8 @@
 name = "THaNServer";
-picture = "THaNServer/Scripts/textures/Canna.edds";
-logoSmall = "THaNServer/Scripts/textures/Canna.edds";
-logo = "THaNServer/Scripts/textures/Canna.edds";
-logoOver = "THaNServer/Scripts/textures/Canna.edds";
+picture = "THaNServer/Textures/Canna.edds";
+logoSmall = "THaNServer/Textures/Canna.edds";
+logo = "THaNServer/Textures/Canna.edds";
+logoOver = "THaNServer/Textures/Canna.edds";
 action = "https://discord.gg/hFGBHtyZJj";
 tooltip = "The Horror at Namalsk Server Mod";
 overview = "Big Dick Energy for those that play this server.";
